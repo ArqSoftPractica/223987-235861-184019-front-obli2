61df2c25a14c67533a8d0db62965634070443825
condesahreyes <hreyes.condesa@gmail.com>
2023-06-13 01:01:54 -0300

    Implemented product subscription

--- src/services/productService.js
+++ src/services/productService.js
@@ -63,3 +63,31 @@ export async function editProduct(data, fileName, onSuccess = null, onError = nu
       executeError(onError, error);
   }
 }
+
+export async function getSuscribeUserProduct(productId, onSuccess = null, onError = null) {
+  try{
+      const response = await axios.get(`/products/subscribe/${productId}`);
+      onSuccess && onSuccess(response);
+      return true;
+  } catch (error) {
+      return false;
+  }
+}
+
+export async function suscribeUserProduct(productId, onSuccess = null, onError = null) {
+  try{
+      const response = await axios.post(`/products/subscribe/${productId}`);
+      onSuccess && onSuccess(response)
+  } catch (error) {
+      executeError(onError, error);
+  }
+}
+
+export async function unsuscribeUserProduct(productId, onSuccess = null, onError = null) {
+  try{
+      const response = await axios.delete(`/products/subscribe/${productId}`);
+      onSuccess && onSuccess(response)
+  } catch (error) {
+      executeError(onError, error);
+  }
+}
